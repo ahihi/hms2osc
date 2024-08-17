daccd3d02e0b41157eec53837ec39a4f82d69e12
miranda <miranda@pulusound.fi>
2024-08-17 20:39:55 +0300

    logging

---
+++ src/hms2osc.rs
@@ -0,0 +1,164 @@
+use std::fmt;
+use std::fs;
+use std::io::{self, Write};
+use std::net::IpAddr;
+use std::path::{Path, PathBuf};
+use std::thread;
+use std::time::Duration;
+
+use directories::ProjectDirs;
+use huelib::{self, bridge::Bridge, resource::sensor::Sensor};
+use log::{error, warn, info, debug, trace};
+use rosc::{OscMessage, OscPacket, OscType};
+use serde::{Serialize, Deserialize};
+use serde_json;
+use thiserror::Error;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum SensorKind {
+    AmbientLight,
+    Presence,
+    Temperature,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct SensorConfig {
+    pub name: String,
+    pub enabled: bool,
+    pub osc_address: String,
+    pub kind: SensorKind,
+}
+
+#[derive(Error, Debug)]
+#[error(transparent)]
+pub enum FromFileError {
+    Io(#[from] io::Error),
+    SerdeJson(#[from] serde_json::Error)
+}
+
+pub fn register_hue_user(bridge_host: IpAddr) -> Result<String,huelib::Error> {
+    let device_type = env!("CARGO_PKG_NAME");
+    info!("registering user on Hue bridge");
+    match huelib::bridge::register_user(bridge_host, device_type) {
+        Err(huelib::Error::Response(e)) if e.kind == huelib::response::ErrorKind::LinkButtonNotPressed => {
+            info!("waiting for the link button to be pressed...");
+            let mut result = None;
+            while result.is_none() {
+                result = match huelib::bridge::register_user(bridge_host, device_type) {
+                    Err(huelib::Error::Response(e)) if e.kind == huelib::response::ErrorKind::LinkButtonNotPressed => {
+                        thread::sleep(Duration::from_secs(1));
+                        None
+                    },
+                    r => Some(r)
+                }
+            }
+            result.unwrap()
+        },
+        r => r
+    }
+}
+
+pub fn default_hue_user_file_path() -> PathBuf {
+    let proj_dirs = ProjectDirs::from("fi", "pulusound", env!("CARGO_PKG_NAME")).unwrap();
+    let path = proj_dirs.data_dir().join("username.txt");
+    path
+}
+
+pub fn ensure_hue_user<P: AsRef<Path>>(bridge_host: IpAddr, user_file_path: P) -> Result<String,EnsureHueUserError> {
+    let username = match fs::read_to_string(&user_file_path) {
+        Err(e) if e.kind() == io::ErrorKind::NotFound => {
+            let name = register_hue_user(bridge_host)?;
+
+            let parent_dir = user_file_path.as_ref().parent().unwrap();
+            fs::create_dir_all(&parent_dir)?;
+            let mut file = fs::File::create(&user_file_path)?;
+            write!(file, "{}", name)?;
+
+            Ok(name)
+        },
+        r => r
+    }?;
+    Ok(username)
+}
+
+#[derive(Error, Debug)]
+#[error(transparent)]
+#[non_exhaustive]
+pub enum EnsureHueUserError {
+    Hue(#[from] huelib::Error),
+    Io(#[from] io::Error)
+}
+
+#[derive(Debug)]
+pub struct SensorTransformer {
+    pub id: String,
+    pub sensor_config: SensorConfig,
+    pub osc_packet: OscPacket,
+}
+
+impl SensorTransformer {
+    pub fn new(id: String, sensor_config: SensorConfig) -> SensorTransformer {
+        Self {
+            id,
+            sensor_config: sensor_config.clone(),
+            osc_packet: OscPacket::Message(OscMessage {
+                addr: sensor_config.osc_address,
+                args: Vec::new()
+            })
+        }
+    }
+
+    pub fn update(&mut self, bridge: &Bridge) -> Result<(), huelib::Error> {
+        let OscPacket::Message(ref mut msg) = self.osc_packet else {
+            unreachable!();
+        };
+        msg.args.clear();
+        let sensor = bridge.get_sensor(&self.id)?;
+        debug!("update sensor {} ({})", self.id, self.sensor_config.name);
+
+        match self.sensor_config.kind {
+            SensorKind::AmbientLight => {
+                let light_level = sensor.state.light_level.unwrap();
+                let dark = sensor.state.dark.unwrap();
+                let daylight = sensor.state.daylight.unwrap();
+                let lux = 10.0_f32.powf((light_level - 1) as f32 / 10000.0);
+
+                msg.args.push(OscType::Float(lux));
+                msg.args.push(OscType::Float(if dark { 1.0 } else { 0.0 }));
+                msg.args.push(OscType::Float(if daylight { 1.0 } else { 0.0 }));
+            },
+            SensorKind::Presence => {
+                let presence = sensor.state.presence.unwrap();
+
+                msg.args.push(OscType::Float(if presence { 1.0 } else { 0.0 }));
+            },
+            SensorKind::Temperature => {
+                let temperature = sensor.state.temperature.unwrap();
+
+                msg.args.push(OscType::Float(temperature as f32 / 100.0));
+            },
+        }
+
+        debug!("osc: {:?}", msg);
+
+        Ok(())
+    }
+}
+
+impl fmt::Display for SensorTransformer {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let sc = &self.sensor_config;
+        write!(f, "{} ({}), kind={:?} -> {}", sc.name, self.id, sc.kind, sc.osc_address)
+    }
+}
+
+pub fn prepare_sensor_transformers<'a>(sensors: &'a [Sensor], sensor_configs: &'a [SensorConfig]) -> impl Iterator<Item = SensorTransformer> + 'a {
+    sensor_configs.iter()
+        .filter(|sensor_config| sensor_config.enabled)
+        .map(|sensor_config| {
+            let id = sensors.iter()
+                .filter(|s| s.name == sensor_config.name)
+                .next().unwrap().id.clone();
+            SensorTransformer::new(id, sensor_config.clone())
+        })
+}
--- src/main.rs
+++ src/main.rs
@@ -1,160 +1,68 @@
-use std::env;
-use std::fs::{self, File};
-use std::io::{self, BufReader, Write};
+use std::fs::File;
+use std::io::BufReader;
 use std::net::{IpAddr, Ipv4Addr, SocketAddrV4, UdpSocket};
-use std::path::PathBuf;
 use std::thread;
 use std::time::Duration;
 
-use directories::ProjectDirs;
-use huelib::{self, resource::sensor::Sensor};
-use rosc::{encoder, OscMessage, OscPacket, OscType};
+use colog;
+use huelib;
+use log::{error, warn, info, debug, trace};
+use rosc;
 use serde::{Serialize, Deserialize};
 use serde_json;
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub enum SensorKind {
-    AmbientLight,
-    Presence,
-    Temperature,
-}
-
-impl SensorKind {
-    pub fn push_data(&self, sensor: &Sensor, args: &mut Vec<OscType>) -> () {
-        match self {
-            SensorKind::AmbientLight => {
-                let light_level = sensor.state.light_level.unwrap();
-                let dark = sensor.state.dark.unwrap();
-                let daylight = sensor.state.daylight.unwrap();
-                let lux = 10.0_f32.powf((light_level - 1) as f32 / 10000.0);
-
-                args.push(OscType::Float(lux));
-                args.push(OscType::Float(if dark { 1.0 } else { 0.0 }));
-                args.push(OscType::Float(if daylight { 1.0 } else { 0.0 }));
-            },
-            SensorKind::Presence => {
-                let presence = sensor.state.presence.unwrap();
-
-                args.push(OscType::Float(if presence { 1.0 } else { 0.0 }));
-            },
-            SensorKind::Temperature => {
-                let temperature = sensor.state.temperature.unwrap();
-
-                args.push(OscType::Float(temperature as f32 / 100.0));
-            },
-        }
-    }
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct SensorConfig {
-    pub name: String,
-    pub enabled: bool,
-    pub osc_address: String,
-    pub kind: SensorKind,
-}
+mod hms2osc;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Config {
     pub bridge_host: IpAddr,
     pub osc_out_addr: SocketAddrV4,
     pub poll_interval: f32,
-    pub sensors: Vec<SensorConfig>,
-}
-
-fn config_path() -> io::Result<PathBuf> {
-    let mut dir = env::current_exe()?;
-    dir.pop();
-    dir.push("config.json");
-    Ok(dir)
+    pub sensors: Vec<hms2osc::SensorConfig>,
 }
 
 fn main() {
-    let config_file_path = "./config.json";//config_path().unwrap();
-    println!("config_file_path: {:?}", config_file_path);
+    let mut colog_builder = colog::default_builder();
+    // if let Some(ref filters_str) = options.log {
+    //     colog_builder.parse_filters(filters_str);
+    // }
+    colog_builder.init();
+
+    let config_file_path = "./config.json";
+    info!("config file path: {:?}", config_file_path);
+
     let config_file = File::open(&config_file_path).unwrap();
     let config_reader = BufReader::new(config_file);
     let config: Config = serde_json::from_reader(config_reader).unwrap();
+    info!("bridge host: {}", config.bridge_host);
+    info!("osc output address: {}", config.osc_out_addr);
 
-    let addr = config.bridge_host;
-    // let sensor_name = "Hue motion sensor 1";
-    let poll_interval = Duration::from_secs_f32(config.poll_interval);
+    info!("connecting to bridge");
+    let user_file_path = hms2osc::default_hue_user_file_path();
+    let username = hms2osc::ensure_hue_user(config.bridge_host, user_file_path).unwrap();
 
-    let proj_dirs = ProjectDirs::from("fi", "pulusound", "hms2osc").unwrap();
-    let username_path = proj_dirs.data_dir().join("username");
-
-    let username = match fs::read_to_string(&username_path) {
-        Ok(u) => {
-            println!("connecting to Hue bridge with username from {:?}", username_path);
-            u
-        },
-        Err(e) if e.kind() == io::ErrorKind::NotFound => {
-            fs::create_dir_all(&username_path.parent().unwrap()).unwrap();
-            let name = register_hue_user(addr).unwrap();
-            let mut file = fs::File::create(&username_path).unwrap();
-            write!(file, "{}", name).unwrap();
-            name
-        },
-        e@Err(_) => e.unwrap()
-    };
-
-    let bridge = huelib::bridge::Bridge::new(addr, username);
+    let bridge = huelib::bridge::Bridge::new(config.bridge_host, username);
     let all_sensors = bridge.get_all_sensors().unwrap();
-    println!("all_sensors: {:?}", all_sensors);
-    let mut sensors = config.sensors.iter()
-        .filter(|sensor_config| sensor_config.enabled)
-        .map(|sensor_config| {
-
-            let id = &all_sensors.iter()
-                .filter(|s| s.name == sensor_config.name)
-                .next().unwrap().id;
-            let osc_packet = OscPacket::Message(OscMessage {
-                addr: sensor_config.osc_address.clone(),
-                args: Vec::new()
-            });
-            (id, sensor_config, osc_packet)
-        })
+    let mut sensor_tfs = hms2osc::prepare_sensor_transformers(&all_sensors, &config.sensors)
         .collect::<Vec<_>>();
 
-    let host_addr = SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), 0);
-    let sock = UdpSocket::bind(host_addr).unwrap();
+    let sensors_str = sensor_tfs.iter()
+        .map(|sensor_tf| format!("{}", sensor_tf))
+        .collect::<Vec<_>>().join("\n");
+    info!("enabled sensors:\n{}", sensors_str);
 
+    let sock = UdpSocket::bind(SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), 0)).unwrap();
+    let poll_interval = Duration::from_secs_f32(config.poll_interval);
+
+    info!("polling sensors every {} seconds", poll_interval.as_secs());
     loop {
-        for (id, sensor_config, ref mut osc_packet) in sensors.iter_mut() {
-            let OscPacket::Message(ref mut osc_msg) = osc_packet else {
-                unreachable!();
-            };
-            osc_msg.args.clear();
-            let sensor = bridge.get_sensor(*id).unwrap();
-            println!("sensor {} ({})", sensor_config.name, id);
-            sensor_config.kind.push_data(&sensor, &mut osc_msg.args);
-            println!("osc_msg: {:?}", osc_msg);
+        for sensor_tf in sensor_tfs.iter_mut() {
+            sensor_tf.update(&bridge).unwrap();
 
-            let msg_buf = encoder::encode(&osc_packet).unwrap();
+            let msg_buf = rosc::encoder::encode(&sensor_tf.osc_packet).unwrap();
             sock.send_to(&msg_buf, config.osc_out_addr).unwrap();
         }
-        thread::sleep(poll_interval);
-    }
-}
 
-fn register_hue_user(addr: IpAddr) -> huelib::Result<String> {
-    let device_type = "hms2osc";
-    println!("registering user on Hue bridge");
-    match huelib::bridge::register_user(addr, device_type) {
-        Err(huelib::Error::Response(e)) if e.kind == huelib::response::ErrorKind::LinkButtonNotPressed => {
-            println!("waiting for the link button to be pressed");
-            let mut result = None;
-            while result.is_none() {
-                result = match huelib::bridge::register_user(addr, device_type) {
-                    Err(huelib::Error::Response(e)) if e.kind == huelib::response::ErrorKind::LinkButtonNotPressed => {
-                        thread::sleep(Duration::from_secs(1));
-                        None
-                    },
-                    r => Some(r)
-                }
-            }
-            result.unwrap()
-        },
-        r => r
+        thread::sleep(poll_interval);
     }
 }
